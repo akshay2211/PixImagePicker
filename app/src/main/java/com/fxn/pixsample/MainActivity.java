db3c61014325c37b05c9782d10163f319ea207d0
Akshay Sharma <fxn769@gmail.com>
2018-06-19 15:44:30 +0530

    Handling onActivtyResults

--- app/src/main/java/com/fxn/pixsample/MainActivity.java
+++ app/src/main/java/com/fxn/pixsample/MainActivity.java
@@ -2,11 +2,13 @@ package com.fxn.pixsample;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.fxn.adapters.MyAdapter;
 import com.fxn.pix.Pix;
@@ -47,9 +49,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
             case PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
-                Pix.start(MainActivity.this, 100, 5);
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Pix.start(MainActivity.this, 100, 5);
+                } else {
+                    Toast.makeText(MainActivity.this, "Approve permissions to open Pix ImagePicker", Toast.LENGTH_LONG).show();
+                }
+                return;
             }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
         }
     }
 }
