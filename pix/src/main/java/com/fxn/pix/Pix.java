d14eb60597be5e4a26f72255569cfa6b2801cdf3
Akshay Sharma <fxn769@gmail.com>
2018-07-10 11:28:23 +0530

    core library updated

--- pix/src/main/java/com/fxn/pix/Pix.java
+++ pix/src/main/java/com/fxn/pix/Pix.java
@@ -55,7 +55,7 @@ import java.util.Set;
 
 import io.fotoapparat.Fotoapparat;
 import io.fotoapparat.configuration.CameraConfiguration;
-import io.fotoapparat.log.LoggersKt;
+import io.fotoapparat.parameter.ScaleType;
 import io.fotoapparat.result.BitmapPhoto;
 import io.fotoapparat.selector.FlashSelectorsKt;
 import io.fotoapparat.selector.FocusModeSelectorsKt;
@@ -75,7 +75,7 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
     public static float TOPBAR_HEIGHT;
     int BottomBarHeight = 0;
     int colorPrimaryDark;
-    CameraConfiguration cameraConfiguration;
+
     Fotoapparat fotoapparat;
     private Handler handler = new Handler();
     private FastScrollStateChangeListener mFastScrollStateChangeListener;
@@ -348,26 +348,25 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
         colorPrimaryDark = ResourcesCompat.getColor(getResources(), R.color.colorPrimaryPix, getTheme());
         mCamera = findViewById(R.id.camera_view);
         fotoapparat = Fotoapparat.with(this).into(mCamera)
-                //.previewScaleType(ScaleType.CenterCrop)  // we want the preview to fill the view
-                //   .photoResolution(ResolutionSelectorsKt.lowestResolution())   // we want to have the biggest photo possible
+                .previewScaleType(ScaleType.CenterCrop)  // we want the preview to fill the view
+                // .photoResolution(ResolutionSelectorsKt.lowestResolution())   // we want to have the biggest photo possible
                 .lensPosition(LensPositionSelectorsKt.back())      // we want back camera
-                .focusMode(FocusModeSelectorsKt.autoFocus())
-                .flash(SelectorsKt.firstAvailable(      // (optional) similar to how it is done for focus mode, this time for flash
-                        FlashSelectorsKt.autoRedEye(),
-                        FlashSelectorsKt.autoFlash(),
-                        FlashSelectorsKt.torch()
+                .focusMode(SelectorsKt.firstAvailable(  // (optional) use the first focus mode which is supported by device
+                        FocusModeSelectorsKt.continuousFocusPicture(),
+                        FocusModeSelectorsKt.autoFocus(),    // in case if continuous focus is not available on device, auto focus will be used
+                        FocusModeSelectorsKt.fixed()             // if even auto focus is not available - fixed focus mode will be used
                 ))
-                .logger(LoggersKt.loggers(            // (optional) we want to log camera events in 2 places at once
+                //.flash(FlashSelectorsKt.autoRedEye())
+                /*.logger(LoggersKt.loggers(            // (optional) we want to log camera events in 2 places at once
                         LoggersKt.logcat(),           // ... in logcat
                         LoggersKt.fileLogger(this)    // ... and to file
-                ))
+                ))*/
                 .build();
 
         fotoapparat.start();
         fotoapparat.autoFocus();
-        cameraConfiguration = new CameraConfiguration();
-        CameraConfiguration.builder().flash(FlashSelectorsKt.autoFlash()).build();
-        fotoapparat.updateConfiguration(cameraConfiguration);
+        CameraConfiguration cameraConfiguration = new CameraConfiguration();
+        fotoapparat.updateConfiguration(CameraConfiguration.builder().flash(FlashSelectorsKt.autoRedEye()).build());
 
         clickme = findViewById(R.id.clickme);
         flash = findViewById(R.id.flash);
@@ -426,10 +425,12 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
         clickme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 fotoapparat.takePicture().toBitmap().transform(new Function1<BitmapPhoto, Bitmap>() {
                     @Override
                     public Bitmap invoke(BitmapPhoto bitmapPhoto) {
                         Log.e("my pick transform", bitmapPhoto.toString());
+                        fotoapparat.stop();
                         return Utility.rotate(bitmapPhoto.bitmap, -bitmapPhoto.rotationDegrees);
                     }
                 }).whenAvailable(new Function1<Bitmap, Unit>() {
@@ -442,10 +443,48 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
                             selectionList.clear();
                             selectionList.add(new Img("", "", photo.getAbsolutePath(), ""));
                             returnObjects();
+
                         }
                         return null;
                     }
                 });
+             /*   try {
+
+
+                    File photo = Utility.writeImage(fotoapparat.takePicture().toBitmap().transform(new Function1<BitmapPhoto, Bitmap>() {
+                        @Override
+                        public Bitmap invoke(BitmapPhoto bitmapPhoto) {
+                            Log.e("my pick transform", bitmapPhoto.toString());
+                            return Utility.rotate(bitmapPhoto.bitmap, -bitmapPhoto.rotationDegrees);
+                        }
+                    }).await());
+                    Log.e("my pick saved",   "    ->  " + photo.length() / 1024);
+                    selectionList.clear();
+                    selectionList.add(new Img("", "", photo.getAbsolutePath(), ""));
+                    returnObjects();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+
+
+           /*.whenAvailable(new Function1<BitmapPhoto, Unit>() {
+                    @Override
+                    public Unit invoke(BitmapPhoto bitmapPhoto) {
+                        Log.e("my pick", bitmapPhoto.toString());
+                        synchronized (bitmapPhoto) {
+                            File photo = Utility.writeImage(bitmapPhoto.bitmap);
+                            Log.e("my pick saved", bitmapPhoto.toString());
+                            selectionList.clear();
+                            selectionList.add(new Img("", "", photo.getAbsolutePath(), ""));
+                            returnObjects();
+                            return null;
+                        }
+                    }
+                })*/
+
+
             }
         });
         selection_ok.setOnClickListener(new View.OnClickListener() {
@@ -483,36 +522,31 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
         });
         final ImageView iv = (ImageView) flash.getChildAt(0);
 
-        flashDrawable = R.drawable.ic_flash_auto_black_24dp;
+        flashDrawable = R.drawable.ic_flash_off_black_24dp;
         flash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                final CameraConfiguration cameraConfiguration = new CameraConfiguration();
                 final int height = flash.getHeight();
                 iv.animate().translationY(height).setDuration(100).setListener(new AnimatorListenerAdapter() {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
                         iv.setTranslationY(-(height / 2));
-                        Log.e("getFlashMode", " -> " + cameraConfiguration.getFlashMode().hashCode() + "   --   " +
-                                FlashSelectorsKt.autoFlash().hashCode() + "   --  " + FlashSelectorsKt.off().hashCode()
-                        );
                         if (flashDrawable == R.drawable.ic_flash_auto_black_24dp) {
                             flashDrawable = R.drawable.ic_flash_off_black_24dp;
                             iv.setImageResource(flashDrawable);
-                            CameraConfiguration.builder().flash(FlashSelectorsKt.off()).build();
-                            fotoapparat.updateConfiguration(cameraConfiguration);
+                            fotoapparat.updateConfiguration(CameraConfiguration.builder().flash(FlashSelectorsKt.off()).build());
                         } else if (flashDrawable == R.drawable.ic_flash_off_black_24dp) {
                             flashDrawable = R.drawable.ic_flash_on_black_24dp;
                             iv.setImageResource(flashDrawable);
-                            CameraConfiguration.builder().flash(FlashSelectorsKt.on()).build();
-                            fotoapparat.updateConfiguration(cameraConfiguration);
+                            fotoapparat.updateConfiguration(CameraConfiguration.builder().flash(FlashSelectorsKt.on()).build());
                         } else {
                             flashDrawable = R.drawable.ic_flash_auto_black_24dp;
                             iv.setImageResource(flashDrawable);
-                            CameraConfiguration.builder().flash(FlashSelectorsKt.autoFlash()).build();
-                            fotoapparat.updateConfiguration(cameraConfiguration);
+                            fotoapparat.updateConfiguration(CameraConfiguration.builder().flash(FlashSelectorsKt.autoRedEye()).build());
                         }
+                        // fotoapparat.focus();
                         iv.animate().translationY(0).setDuration(50).setListener(null).start();
                     }
                 }).start();
@@ -522,6 +556,7 @@ public class Pix extends AppCompatActivity implements View.OnTouchListener {
         front.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final CameraConfiguration cameraConfiguration = new CameraConfiguration();
                 final ObjectAnimator oa1 = ObjectAnimator.ofFloat(front, "scaleX", 1f, 0f).setDuration(150);
                 final ObjectAnimator oa2 = ObjectAnimator.ofFloat(front, "scaleX", 0f, 1f).setDuration(150);
                 oa1.addListener(new AnimatorListenerAdapter() {
