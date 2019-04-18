0475e1f60a0e9a77ea88f96228235bbabd10f293
Olivier <olivier.peyronnel@gmail.com>
2019-04-18 11:39:40 +0200

    [PixWhatsApp] Revert reot tracking merge issue

--- app/src/main/java/com/fxn/pixsample/MainActivity.java
+++ app/src/main/java/com/fxn/pixsample/MainActivity.java
@@ -6,22 +6,23 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-import com.fxn.adapters.MyAdapter;
-import com.fxn.pix.Options;
-import com.fxn.pix.Pix;
-import com.fxn.utility.PermUtil;
-
-import java.util.ArrayList;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.fxn.adapters.MyAdapter;
+import com.fxn.pix.Options;
+import com.fxn.pix.Pix;
+import com.fxn.utility.ImageQuality;
+import com.fxn.utility.PermUtil;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     MyAdapter myAdapter;
-    private ArrayList<String> previouslySelectedPathList = new ArrayList<>();
+    Options options;
+    ArrayList<String> returnValue = new ArrayList<>();
+    ArrayList<String> returnValue1 = new ArrayList<>();
+    ArrayList<String> previouslySelectedPathList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +31,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         myAdapter = new MyAdapter(this);
+        options = Options.init()
+                .setRequestCode(100)
+                .setCount(3)
+                .setFrontfacing(true)
+                .setImageQuality(ImageQuality.HIGH)
+                .setImageResolution(1024, 800)
+                .setPreSelectedUrls(returnValue1)
+                .setScreenOrientation(Options.SCREEN_ORIENTATION_PORTRAIT)
+                .setPath("/akshay/new")
+                .setPreviouslySelectedPathList(previouslySelectedPathList)
+        ;
         recyclerView.setAdapter(myAdapter);
-        findViewById(R.id.fab).setOnClickListener((View view) ->
-                Pix.start(MainActivity.this, Options.init().setRequestCode(100).setCount(2).setFrontfacing(true).setPreviouslySelectedPathList(previouslySelectedPathList)));
+        findViewById(R.id.fab).setOnClickListener((View view) -> {
+            options.setPreSelectedUrls(returnValue1);
+            Pix.start(MainActivity.this, options);
+        });
 
     }
 
@@ -43,9 +57,11 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case (100): {
                 if (resultCode == Activity.RESULT_OK) {
-                    ArrayList<String> returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS);
-                    myAdapter.addImage(returnValue);
+                    returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS);
+                    returnValue1.addAll(returnValue);
                     previouslySelectedPathList.addAll(returnValue);
+                    //    Log.e("count preSelectedlist", "->     " + returnValue1.size());
+                    myAdapter.addImage(returnValue1);
                     /*for (String s : returnValue) {
                         Log.e("val", " ->  " + s);
                     }*/
@@ -61,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             case PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Pix.start(MainActivity.this, Options.init().setRequestCode(100).setCount(1));
+                    Pix.start(MainActivity.this, options);
                 } else {
                     Toast.makeText(MainActivity.this, "Approve permissions to open Pix ImagePicker", Toast.LENGTH_LONG).show();
                 }
--- pix/src/main/java/com/fxn/utility/ImageFetcher.java
+++ pix/src/main/java/com/fxn/utility/ImageFetcher.java
@@ -5,9 +5,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
-
 import com.fxn.modals.Img;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,46 +16,105 @@ import java.util.Locale;
  * Created by akshay on 06/04/18.
  */
 
-public class ImageFetcher extends AsyncTask<Cursor, Void, ArrayList<Img>> {
-    private ArrayList<Img> LIST = new ArrayList<>();
+public class ImageFetcher extends AsyncTask<Cursor, Void, ImageFetcher.ModelList> {
+
+
+    private ArrayList<Img> selectionList = new ArrayList<>();
     private List<String> previouslySelectedPathList = new ArrayList<>();
+  public int startingCount = 0;
+  private ArrayList<Img> LIST = new ArrayList<>();
+
+  public int getStartingCount() {
+    return startingCount;
+  }
+
+  public void setStartingCount(int startingCount) {
+    this.startingCount = startingCount;
+  }
+    private ArrayList<String> preSelectedUrls = new ArrayList<>();
+
+    public ArrayList<String> getPreSelectedUrls() {
+        return preSelectedUrls;
+    }
+
+    public ImageFetcher setPreSelectedUrls(ArrayList<String> preSelectedUrls) {
+        this.preSelectedUrls = preSelectedUrls;
+        return this;
+    }
+
+    @Override
+    protected ModelList doInBackground(Cursor... cursors) {
+        Cursor cursor = cursors[0];
+        try {
+            if (cursor != null) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+                int date = cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN);
+                int data = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                int contentUrl = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                String header = "";
+                int limit = 100;
+                if (cursor.getCount() < 100) {
+                    limit = cursor.getCount() - 1;
+                }
+                cursor.move(limit);
+                synchronized (context) {
+                  int pos = getStartingCount();
+
+                    for (int i = limit; i < cursor.getCount(); i++) {
+                        cursor.moveToNext();
+                        Uri path = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + cursor.getInt(contentUrl));
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTimeInMillis(cursor.getLong(date));
+                        String dateDifference = Utility.getDateDifference(context, calendar);
+                        boolean isPrevisoulySelectedPix = previouslySelectedPathList.contains(cursor.getString(data));
+
+
+                        if (!header.equalsIgnoreCase("" + dateDifference)) {
+                            header = "" + dateDifference;
+                            pos += 1;
+                            LIST.add(new Img("" + dateDifference, "", "", "", isPrevisoulySelectedPix));
+                        }
+                        Img img = new Img("" + header, "" + path, cursor.getString(data), "" + pos, isPrevisoulySelectedPix);
+                        img.setPosition(pos);
+                        if (preSelectedUrls.contains(img.getUrl())) {
+                            img.setSelected(true);
+                            selectionList.add(img);
+                        }
+                        pos += 1;
+                        LIST.add(img);
+                    }
+                    cursor.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ModelList(LIST, selectionList);
+    }
+
     private Context context;
 
     public ImageFetcher(Context context) {
         this.context = context;
     }
 
-    @Override
-    protected ArrayList<Img> doInBackground(Cursor... cursors) {
-        Cursor cursor = cursors[0];
-        if (cursor != null) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
-            int date = cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN);
-            int data = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
-            int contentUrl = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            String header = "";
-            int limit = 100;
-            if (cursor.getCount() < 100) {
-                limit = cursor.getCount();
-            }
-            cursor.move(limit - 1);
-            for (int i = limit; i < cursor.getCount(); i++) {
-                cursor.moveToNext();
-                Uri curl = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + cursor.getInt(contentUrl));
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(cursor.getLong(date));
-                String dateDifference = Utility.getDateDifference(context, calendar);
-                boolean isPrevisoulySelectedPix = previouslySelectedPathList.contains(cursor.getString(data));
+    public class ModelList {
+        ArrayList<Img> LIST = new ArrayList<>();
+        ArrayList<Img> selection = new ArrayList<>();
 
-                if (!header.equalsIgnoreCase(dateDifference)) {
-                    header = dateDifference;
-                    LIST.add(new Img(dateDifference, "", "", dateFormat.format(calendar.getTime()), isPrevisoulySelectedPix));
-                }
-                LIST.add(new Img(header, curl.toString(), cursor.getString(data), dateFormat.format(calendar.getTime()), isPrevisoulySelectedPix));
-            }
-            cursor.close();
+        public ModelList(ArrayList<Img> LIST, ArrayList<Img> selection) {
+            this.LIST = LIST;
+            this.selection = selection;
         }
-        return LIST;
+
+        public ArrayList<Img> getLIST() {
+            return LIST;
+        }
+
+        public ArrayList<Img> getSelection() {
+            return selection;
+        }
     }
 
     public void setPreviouslySelectedPathList(List<String> previouslySelectedPathList) {
@@ -68,4 +125,5 @@ public class ImageFetcher extends AsyncTask<Cursor, Void, ArrayList<Img>> {
             this.previouslySelectedPathList = previouslySelectedPathList;
         }
     }
+
 }
