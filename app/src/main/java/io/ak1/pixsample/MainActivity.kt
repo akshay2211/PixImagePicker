b8b25af1d6f8a9d42d9e0c76daf772da0bb0603d
Akshay Sharma <fxn769@gmail.com>
2021-06-18 22:54:57 +0530

    MainActvity added icon, font added

---
+++ app/src/main/java/io/ak1/pixsample/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.ak1.pixsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Created By Akshay Sharma on 18,June,2021
+ * https://ak1.io
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
