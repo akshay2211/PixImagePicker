b8165ec30bb3860a7cf4acdc35618910138e7164
Akshay Sharma <fxn769@gmail.com>
2021-06-20 20:26:37 +0530

    NavController Sample added

---
+++ app/src/main/java/io/ak1/pixsample/samples/NavControllerSample.kt
@@ -0,0 +1,96 @@
+package io.ak1.pixsample.samples
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.get
+import io.ak1.pix.helpers.PixBus
+import io.ak1.pix.helpers.PixEventCallback
+import io.ak1.pix.helpers.setupScreen
+import io.ak1.pix.helpers.showStatusBar
+import io.ak1.pix.models.Mode
+import io.ak1.pix.models.Options
+import io.ak1.pix.utility.ARG_PARAM_PIX
+import io.ak1.pixsample.R
+import io.ak1.pixsample.commons.Adapter
+import io.ak1.pixsample.custom.fragmentBody
+import io.ak1.pixsample.databinding.ActivityNavControllerSampleBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * Created By Akshay Sharma on 20,June,2021
+ * https://ak1.io
+ */
+
+class NavControllerSample : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityNavControllerSampleBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNavControllerSampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        setupScreen()
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        PixBus.results {
+            when (it.status) {
+                PixEventCallback.Status.SUCCESS -> {
+                    showStatusBar()
+                    navController.navigateUp()
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination == navController.graph[R.id.CameraFragment]) {
+            PixBus.onBackPressedEvent()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    inner class ResultsFragment : Fragment() {
+        private val recyclerViewAdapter = Adapter()
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            PixBus.results(coroutineScope = CoroutineScope(Dispatchers.Main)) {
+                when (it.status) {
+                    PixEventCallback.Status.SUCCESS -> {
+                        recyclerViewAdapter.apply {
+                            this.list.clear()
+                            this.list.addAll(it.data)
+                            notifyDataSetChanged()
+                        }
+                    }
+                    PixEventCallback.Status.BACK_PRESSED -> {
+                        requireActivity().onBackPressed()
+                    }
+                }
+            }
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View = fragmentBody(requireActivity(), recyclerViewAdapter) {
+            var bundle = bundleOf(ARG_PARAM_PIX to Options().apply {
+                isFrontFacing = false
+                mode = Mode.All
+                count = 5
+            })
+            findNavController().navigate(R.id.action_ResultsFragment_to_CameraFragment, bundle)
+        }
+    }
+}
